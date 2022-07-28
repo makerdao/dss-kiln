81f05b80f9cbe0d2338207889f9649e5f94fd47c
Brian McMichael <brian@brianmcmichael.com>
2022-07-28 12:11:52 -0400

    0.8.14

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-all    :; DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 forge build --use solc:0.8.13
+all    :; DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 forge build --use solc:0.8.14
 clean  :; dapp clean
 test   :; make && ./test.sh $(match)
--- src/DssKiln.sol
+++ src/DssKiln.sol
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.14;
 
 interface GemLike {
     function approve(address, uint256) external returns (bool);
