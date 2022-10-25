1f198ef97eecd4799f213642100dc5bba0edb244
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-10-25 10:10:46 -0300

    Fix license in files

--- src/DssKiln.sol
+++ src/DssKiln.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // DssKiln - Asset acquisition and control module
 //
--- src/DssKilnUNIV3BurnStrategy.sol
+++ src/DssKilnUNIV3BurnStrategy.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // DssKilnUNIV3BurnStrategy - Burn Module for Uniswap V3
 //
